945084a78873d39132981fdbce0f7c03746b4f05
nicfheorais <7238289+nicfheorais@users.noreply.github.com>
2022-02-10 12:45:53 +0000

    fixed bug - needed function to be called with props when rendering square

--- src/Components/Board/index.js
+++ src/Components/Board/index.js
@@ -13,22 +13,15 @@ import "./board.css";
 import Square from "../Square";
 
 function Board({ boardArray, handlePlayerMove }) {
-    // console.log(`In Board Component`);
-
-    //let playerMoveSymbol = "X"; // NO LONGER NEEDED - map() takes care of this.
-    // let handlePlayerMove = () => {}; //TODO: move to Game?
-
     //Note: although it's usually a bad idea to use Array index as a React key. Here it is okay because the array will laoways be exactly 9 values long. Records will not be added or removed during the game, only overwritten.
-    //TODO: may need to change the handlePlayerMove callback function into handlePlayerMove={() => handlePlayerMove(props)} if we need to pass 'owt in.
     return (
         <div className="board">
-            {/* <p>DEBUG: render Board stuff in here.</p> */}
-            {boardArray.map((playerMoveSymbol, currIndex) => {
+            {boardArray.map((currentPlayerSymbol, currIndex) => {
                 return (
                     <Square
                         key={currIndex}
-                        playerMoveSymbol={playerMoveSymbol}
-                        handlePlayerMove={handlePlayerMove}
+                        currentPlayerSymbol={currentPlayerSymbol}
+                        handlePlayerMove={() => handlePlayerMove(currIndex)}
                     />
                 );
             })}
--- src/Components/Square/index.js
+++ src/Components/Square/index.js
@@ -8,20 +8,17 @@
 //         - box => X, O, empty
 import "./square.css";
 
-function Square({ playerMoveSymbol, handlePlayerMove }) {
-    // console.log(`In Square Component`);
-
-    const style = playerMoveSymbol
-        ? `squares-button ${playerMoveSymbol}-button`
+function Square({ currentPlayerSymbol, handlePlayerMove }) {
+    const style = currentPlayerSymbol
+        ? `squares-button ${currentPlayerSymbol}-button`
         : `squares-button`;
 
     //TODO:replace button code with a <Button> component to abstract away the nitty-gritty of what attribute names a button needs to work
-
     return (
         <>
             {/* <p>render Square stuff in here.</p> */}
             <button className={style} onClick={handlePlayerMove}>
-                {playerMoveSymbol}
+                {currentPlayerSymbol}
             </button>
         </>
     );
